108f3a9dedd0d156aaa36abd9d2207fdf05f735d
Nitin <nitin.chandekar@stltech.in>
2020-11-19 10:41:46 +0530

    test

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 error_reporting(0);
- include_once('constant.php');
-$servername ="localhost";
-$username = "root";
-$password = "";
-$dbname = "test_db";
+include_once('constant.php');
+$servername ="45.87.80.103";
+$username = "u904913351_test_nitin";
+$password = "Java@123";
+$dbname = "u904913351_test_nitin";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
