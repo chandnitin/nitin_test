06502038c60ba7c1198c915ef661b74e58320446
Nitin <nitin.chandekar@stltech.in>
2020-11-19 13:08:22 +0530

    test

--- constant.php
+++ constant.php
@@ -1,7 +1,7 @@
 <?php
 
 define('STORAGE','uploads/');
-define('BASE_URL','https://nitin-test.herokuapp.com/');
+define('BASE_URL','http://localhost/nitin_test');
 
 define('FB_APP_ID', '170992148022655');
 define('FB_APP_SECRET', 'ecee3700c2763a90272990526ce70436');
